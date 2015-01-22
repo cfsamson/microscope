d771fedadce34a52c3e5a61a4ad0c693571fe686
Carl Fredrik Samson <carlfredriksamson@Carls-MacBook-Pro.local>
2015-01-22 22:00:47 +0100

    Demonstrates the order of posts during sleep

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -15,8 +15,10 @@ Template.postSubmit.events({
                 alert('This link has already been posted');
             }
 
-            Router.go('postPage', {_id: result._id});
+
         });
+
+        Router.go('postPage', {_id: result._id});
 
     }
 });
