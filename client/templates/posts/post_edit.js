74bb6d6067daf46906ef0e8cfcdc29cf92254b78
Carl Fredrik Samson <cf@samson.no>
2015-01-23 16:56:09 +0100

    Validate post content when editing

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -1,29 +1,58 @@
-Template.postEdit.events({
-    'submit form': function (e) {
-        e.preventDefault();
 
+Template.postEdit.created = createdEvent
+Template.postEdit.events({
+    'submit form': submitFormEvent,
+    'click .delete': clickDeleteEvent
+});
+
+Template.postEdit.helpers({
+    errorMessage: errorMessage,
+    errorClass: errorClass
+})
+
+function clickDeleteEvent(e) {
+    e.preventDefault();
+
+    if(confirm('Delete this post?')){
         var currentPostId = this._id;
 
-        var postProperties = {
-            url: $(e.target).find('[name=url]').val(),
-            title: $(e.target).find('[name=title]').val()
-        }
-
-        Posts.update(currentPostId, {$set: postProperties}, function (err) {
-            if(err){ throwError(err.reason);}
-            else{
-                Router.go('postPage', {_id: currentPostId._id});
-            }
-        })
-    },
-    'click .delete': function (e) {
-        e.preventDefault();
-
-        if(confirm('Delete this post?')){
-            var currentPostId = this._id;
-
-            Posts.remove(currentPostId);
-            Router.go('postsList');
-        }
+        Posts.remove(currentPostId);
+        Router.go('postsList');
     }
-})
+}
+
+function createdEvent(){
+    Session.set('postEditErrors', {});
+}
+
+function errorClass(field){
+    return !! Session.get('postEditErrors'[field] ? 'has-error': '');
+}
+
+function errorMessage(field){
+    return Session.get('postEditErrors')[field];
+}
+
+function submitFormEvent(e) {
+    e.preventDefault();
+
+    var currentPostId = this._id;
+
+    var postProperties = {
+        url: $(e.target).find('[name=url]').val(),
+        title: $(e.target).find('[name=title]').val()
+    };
+
+    var errors = validatePost(postProperties);
+    if(errors.title || errors.url){
+        return Session.set('postEditErrors', errors);
+    }
+
+    Posts.update(currentPostId, {$set: postProperties}, function (err) {
+        if(err){
+            throwError(err.reason);
+        } else {
+            Router.go('postPage', {_id: currentPostId._id});
+        }
+    })
+}
