b4d240de7dd0447a8624ea54d8c25f955a09eee2
Carl Fredrik Samson <cf@samson.no>
2015-01-23 16:36:06 +0100

    Validate post contents on submission

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -6,6 +6,11 @@ Template.postSubmit.events({
             url: $(e.target).find('[name=url]').val(),
             title: $(e.target).find('[name=title]').val()
         };
+
+        var errors = validatePost(post);
+        if(errors.title || errors.url){
+            return Session.set('postSubmitErrors', errors);
+        }
         
         Meteor.call('postInsert', post, function (err, result) {
             if(err){
@@ -21,3 +26,21 @@ Template.postSubmit.events({
 
     }
 });
+
+// initlialize postSubmitErrors object when postSubmit-template is created
+Template.postSubmit.created = function () {
+  Session.set('postSubmitErrors', {});
+};
+
+Template.postSubmit.helpers({
+    errorMessage: errorMessage,
+    errorClass: errorClass
+})
+
+function errorClass(field){
+    return !! Session.get('postSubmitErrors')[field] ? 'has-error': '';
+}
+
+function errorMessage(field){
+    return Session.get('postSubmitErrors')[field];
+}
