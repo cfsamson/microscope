777b34ec159589910fa6c43fd652ee8dafd3b847
Carl Fredrik Samson <carlfredriksamson@Carls-MacBook-Pro.local>
2015-01-25 01:36:13 +0100

    Denormalized the number of comments into the post.

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -6,8 +6,5 @@ Template.postItem.helpers({
    },
     'ownPost': function () {
         return this.userId === Meteor.userId();
-    },
-    commentsCount: function () {
-        Comments.find({_id: this._id}).count();
     }
 });
